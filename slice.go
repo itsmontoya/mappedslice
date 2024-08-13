70cc876d06863af32be14fde322e80330fc030f1
Josh Montoya <josh@montoya.io>
2024-08-13 12:16:04 -0700

    Mapped -> Slice

---
+++ slice.go
@@ -0,0 +1,204 @@
+package mapped
+
+import (
+	"fmt"
+	"os"
+	"unsafe"
+
+	"github.com/edsrzf/mmap-go"
+)
+
+const mappedCeiling = 1<<37 - 1
+
+func New[T any](filepath string) (ref *Slice[T], err error) {
+	var d Slice[T]
+	if d.f, err = os.OpenFile(filepath, os.O_CREATE|os.O_RDWR, 0644); err != nil {
+		return
+	}
+
+	var t T
+	d.sizeOf = int64(unsafe.Sizeof(t))
+
+	var info os.FileInfo
+	if info, err = d.f.Stat(); err != nil {
+		return
+	}
+
+	if info.Size() == 0 {
+		if err = d.growTo(32); err != nil {
+			return
+		}
+	} else {
+		d.associate()
+	}
+
+	return &d, nil
+}
+
+type Slice[T any] struct {
+	f  *os.File
+	mm mmap.MMap
+	s  []T
+
+	sizeOf int64
+
+	len *int64
+	cap *int64
+}
+
+func (s *Slice[T]) Get(index int) (kv T, err error) {
+	if err = s.boundsCheck(index); err != nil {
+		return
+	}
+
+	kv = s.s[index]
+	return
+}
+
+func (s *Slice[T]) Set(index int, t T) (err error) {
+	if err = s.boundsCheck(index); err != nil {
+		return
+	}
+
+	s.s[index] = t
+	return
+}
+
+func (s *Slice[T]) Append(t T) (err error) {
+	if err = s.grow(); err != nil {
+		return
+	}
+
+	s.s = append(s.s, t)
+	*s.len++
+	return
+}
+
+func (s *Slice[T]) InsertAt(index int, value T) (err error) {
+	if err = s.boundsCheck(index); err != nil {
+		return
+	}
+
+	if err = s.Append(value); err != nil {
+		return
+	}
+
+	copy(s.s[index+1:], s.s[index:])
+	s.s[index] = value
+	*s.len++
+	return
+}
+
+func (s *Slice[T]) RemoveAt(index int) (err error) {
+	if err = s.boundsCheck(index); err != nil {
+		return
+	}
+
+	first := s.s[:index]
+	second := s.s[index+1:]
+	s.s = append(first, second...)
+	*s.len--
+	return
+}
+
+func (s *Slice[T]) ForEach(fn func(T) (end bool)) (ended bool) {
+	for _, t := range s.s {
+		if ended = fn(t); ended {
+			return
+		}
+	}
+
+	return
+}
+
+func (s *Slice[T]) Len() int {
+	return len(s.s)
+}
+
+func (s *Slice[T]) Slice() (out []T) {
+	out = make([]T, len(s.s))
+	copy(out, s.s)
+	return
+}
+
+func (s *Slice[T]) Close() (err error) {
+	if err = s.unmap(); err != nil {
+		return
+	}
+
+	return s.f.Close()
+}
+
+func (s *Slice[T]) unmap() (err error) {
+	if s.mm == nil {
+		return
+	}
+
+	if err = s.mm.Unmap(); err != nil {
+		return
+	}
+
+	s.mm = nil
+	return
+}
+
+func (s *Slice[T]) associate() (err error) {
+	if s.mm, err = mmap.Map(s.f, os.O_RDWR, 0); err != nil {
+		return
+	}
+
+	s.len = (*int64)(unsafe.Pointer(&s.mm[0]))
+	s.cap = (*int64)(unsafe.Pointer(&s.mm[8]))
+	s.s = (*(*[mappedCeiling]T)(unsafe.Pointer(&s.mm[16])))[:*s.len]
+	return
+}
+
+func (s *Slice[T]) grow() (err error) {
+	if *s.len < *s.cap {
+		return
+	}
+
+	newCapacity := *s.cap * 2
+	if newCapacity == 0 {
+		newCapacity = 32
+	}
+
+	return s.growTo(newCapacity)
+}
+
+func (s *Slice[T]) growTo(newCapacity int64) (err error) {
+	if err = s.unmap(); err != nil {
+		return
+	}
+
+	newSize := 16 + newCapacity*s.sizeOf
+	if err = s.f.Truncate(newSize); err != nil {
+		return
+	}
+
+	if err = s.associate(); err != nil {
+		return
+	}
+
+	*s.cap = newCapacity
+	return
+}
+
+func (s *Slice[T]) boundsCheck(index int) (err error) {
+	if s.isInBounds(index) {
+		return
+	}
+
+	return fmt.Errorf("index of <%d> is out of bounds with a length of <%d>", index, *s.len)
+}
+
+func (s *Slice[T]) isInBounds(index int) (ok bool) {
+	switch {
+	case index > int(*s.len):
+		return false
+	case index < 0:
+		return false
+	default:
+		return true
+	}
+}
---
+++ slice_test.go
@@ -0,0 +1,486 @@
+package mapped
+
+import (
+	"os"
+	"reflect"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	type args struct {
+		filepath string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "basic",
+			args: args{
+				filepath: "test.bat",
+			},
+			wantErr: false,
+		},
+		{
+			name: "no filepath",
+			args: args{
+				filepath: "",
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ref, err := New[int](tt.args.filepath)
+			if err == nil {
+				defer os.Remove(tt.args.filepath)
+				defer ref.Close()
+			}
+
+			if (err != nil) != tt.wantErr {
+				t.Errorf("New() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
+
+func TestSlice_Get(t *testing.T) {
+	type args struct {
+		index int
+	}
+
+	tests := []struct {
+		name            string
+		numberOfEntries int
+		args            args
+
+		want    int
+		wantErr bool
+	}{
+		{
+			name:            "basic",
+			numberOfEntries: 3,
+			args: args{
+				index: 2,
+			},
+			want:    2,
+			wantErr: false,
+		},
+		{
+			name:            "large set",
+			numberOfEntries: 128,
+			args: args{
+				index: 127,
+			},
+			want:    127,
+			wantErr: false,
+		},
+		{
+			name:            "negative index",
+			numberOfEntries: 3,
+			args: args{
+				index: -1,
+			},
+			want:    0,
+			wantErr: true,
+		},
+		{
+			name:            "out of bounds index",
+			numberOfEntries: 3,
+			args: args{
+				index: 5,
+			},
+			want:    0,
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m, err := getTestSlice(tt.numberOfEntries)
+			if err != nil {
+				t.Errorf("Slice.Get(): error preparing: %v", err)
+				return
+			}
+			defer os.Remove(m.f.Name())
+
+			got, err := m.Get(tt.args.index)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Slice.Get() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if got != tt.want {
+				t.Errorf("Slice.Get() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSlice_Set(t *testing.T) {
+	type args struct {
+		index int
+		value int
+	}
+
+	tests := []struct {
+		name            string
+		numberOfEntries int
+		args            args
+
+		want    []int
+		wantErr bool
+	}{
+		{
+			name:            "last index",
+			numberOfEntries: 3,
+			args: args{
+				index: 2,
+				value: 7,
+			},
+			want:    []int{0, 1, 7},
+			wantErr: false,
+		},
+		{
+			name:            "middle index",
+			numberOfEntries: 3,
+			args: args{
+				index: 1,
+				value: 7,
+			},
+			want:    []int{0, 7, 2},
+			wantErr: false,
+		},
+		{
+			name:            "first index",
+			numberOfEntries: 3,
+			args: args{
+				index: 0,
+				value: 7,
+			},
+			want:    []int{7, 1, 2},
+			wantErr: false,
+		},
+		{
+			name:            "negative index",
+			numberOfEntries: 3,
+			args: args{
+				index: -1,
+				value: 7,
+			},
+			want:    []int{0, 1, 2},
+			wantErr: true,
+		},
+		{
+			name:            "out of bounds index",
+			numberOfEntries: 3,
+			args: args{
+				index: 5,
+				value: 7,
+			},
+			want:    []int{0, 1, 2},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m, err := getTestSlice(tt.numberOfEntries)
+			if err != nil {
+				t.Errorf("Slice.Set(): error preparing: %v", err)
+				return
+			}
+			defer os.Remove(m.f.Name())
+
+			err = m.Set(tt.args.index, tt.args.value)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Slice.Set() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			got := m.Slice()
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Slice.Set() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSlice_InsertAt(t *testing.T) {
+	type args struct {
+		index int
+		value int
+	}
+
+	tests := []struct {
+		name            string
+		numberOfEntries int
+		args            args
+
+		want    []int
+		wantErr bool
+	}{
+		{
+			name:            "last index",
+			numberOfEntries: 3,
+			args: args{
+				index: 2,
+				value: 7,
+			},
+			want:    []int{0, 1, 7, 2},
+			wantErr: false,
+		},
+		{
+			name:            "middle index",
+			numberOfEntries: 3,
+			args: args{
+				index: 1,
+				value: 7,
+			},
+			want:    []int{0, 7, 1, 2},
+			wantErr: false,
+		},
+		{
+			name:            "first index",
+			numberOfEntries: 3,
+			args: args{
+				index: 0,
+				value: 7,
+			},
+			want:    []int{7, 0, 1, 2},
+			wantErr: false,
+		},
+		{
+			name:            "negative index",
+			numberOfEntries: 3,
+			args: args{
+				index: -1,
+			},
+			want:    []int{0, 1, 2},
+			wantErr: true,
+		},
+		{
+			name:            "out of bounds index",
+			numberOfEntries: 3,
+			args: args{
+				index: 5,
+			},
+			want:    []int{0, 1, 2},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m, err := getTestSlice(tt.numberOfEntries)
+			if err != nil {
+				t.Errorf("Slice.InsertAt(): error preparing: %v", err)
+				return
+			}
+			defer os.Remove(m.f.Name())
+
+			err = m.InsertAt(tt.args.index, tt.args.value)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Slice.InsertAt() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			got := m.Slice()
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Slice.InsertAt() got = %v, want %v", got, tt.want)
+				return
+			}
+		})
+	}
+}
+
+func TestSlice_RemoveAt(t *testing.T) {
+	type args struct {
+		index int
+	}
+
+	tests := []struct {
+		name            string
+		numberOfEntries int
+		args            args
+
+		want    []int
+		wantErr bool
+	}{
+		{
+			name:            "last index",
+			numberOfEntries: 3,
+			args: args{
+				index: 2,
+			},
+			want:    []int{0, 1},
+			wantErr: false,
+		},
+		{
+			name:            "middle index",
+			numberOfEntries: 3,
+			args: args{
+				index: 1,
+			},
+			want:    []int{0, 2},
+			wantErr: false,
+		},
+		{
+			name:            "first index",
+			numberOfEntries: 3,
+			args: args{
+				index: 0,
+			},
+			want:    []int{1, 2},
+			wantErr: false,
+		},
+		{
+			name:            "negative index",
+			numberOfEntries: 3,
+			args: args{
+				index: -1,
+			},
+			want:    []int{0, 1, 2},
+			wantErr: true,
+		},
+		{
+			name:            "out of bounds index",
+			numberOfEntries: 3,
+			args: args{
+				index: 5,
+			},
+			want:    []int{0, 1, 2},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m, err := getTestSlice(tt.numberOfEntries)
+			if err != nil {
+				t.Errorf("Slice.RemoveAt(): error preparing: %v", err)
+				return
+			}
+			defer os.Remove(m.f.Name())
+
+			err = m.RemoveAt(tt.args.index)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Slice.RemoveAt() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			got := m.Slice()
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Slice.RemoveAt() got = %v, want %v", got, tt.want)
+				return
+			}
+		})
+	}
+}
+
+func TestSlice_ForEach(t *testing.T) {
+	type args struct {
+		end bool
+	}
+
+	tests := []struct {
+		name            string
+		numberOfEntries int
+		args            args
+
+		want      []int
+		wantEnded bool
+	}{
+		{
+			name:            "basic",
+			numberOfEntries: 3,
+			args: args{
+				end: false,
+			},
+			want:      []int{0, 1, 2},
+			wantEnded: false,
+		},
+		{
+			name:            "with end",
+			numberOfEntries: 3,
+			args: args{
+				end: true,
+			},
+			want:      []int{0},
+			wantEnded: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m, err := getTestSlice(tt.numberOfEntries)
+			if err != nil {
+				t.Errorf("Slice.ForEach(): error preparing: %v", err)
+				return
+			}
+			defer os.Remove(m.f.Name())
+
+			var got []int
+			gotEnded := m.ForEach(func(v int) (end bool) {
+				got = append(got, v)
+				return tt.args.end
+			})
+
+			if gotEnded != tt.wantEnded {
+				t.Errorf("Slice.ForEach() gotEnded = %v, wantEnded %v", gotEnded, tt.wantEnded)
+			}
+
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Slice.ForEach() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSlice_Len(t *testing.T) {
+	tests := []struct {
+		name            string
+		numberOfEntries int
+
+		want int
+	}{
+		{
+			name:            "basic",
+			numberOfEntries: 3,
+			want:            3,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m, err := getTestSlice(tt.numberOfEntries)
+			if err != nil {
+				t.Errorf("Slice.Len(): error preparing: %v", err)
+				return
+			}
+			defer os.Remove(m.f.Name())
+
+			got := m.Len()
+			if got != tt.want {
+				t.Errorf("Slice.Len() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func getTestSlice(count int) (m *Slice[int], err error) {
+	if m, err = New[int]("test.bat"); err != nil {
+		return
+	}
+
+	for i := 0; i < count; i++ {
+		if err = m.Append(i); err != nil {
+			return
+		}
+	}
+
+	return
+}
